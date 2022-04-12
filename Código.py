12d31516a43c6bf5c416512f1ce73b4975d9b3e5
ESbeginner <renatto.guiusseppe.peralta@estudiantat.upc.edu>
2022-04-12 18:53:16 +0200

    Hay un total de 17 llaves o variables, estan todas anotadas tal y como lo especifica el archivo

--- Código.py
+++ Código.py
@@ -5,7 +5,9 @@ data = pd.read_csv("Estacions_de_rec_rrega_per_a_vehicle_el_ctric_a_Catalunya.cs
                                                                                     # más que un archivo de python con un <<objeto>>, y este a su vez tiene funciones
                                                                                     # definidas dentro de sí
 df=pd.DataFrame(data) #Lo que hacemos con esta función es cojer el archivo csv, ahora conocido como <<data>>  organizarlo siguiendo un patrón tipo matriz MxN
-df.T #Esta función solo transpone la matriz que acabamos de crear
+df=df.T #Esta función solo transpone la matriz que acabamos de crear
 
 if __name__ == "__main__": # esta sección se encarga de que si somos nosotros lo que ejecutamos el código, se active el condicionante if==True
   print(df) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
+# PROMOTOR-GESTOR, ACCES, TIPUS VELOCITAT, TIPUS CONNEXIÓ, LATITUD, LONGITUD, DESIGNACIÓ-DESCRIPTIVA, POTENCIA, TIPUS DE CORRENT
+# IDENTIFICADOR, ADREÇA, PROVINCIA, CODIPROV, MUNICIPI, NPLACES ESTACIÓ, TIPUS VEHICLE, Columna amb georeferència POINT.
