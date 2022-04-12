0e6d3068db19ff93f4e7960605b80bc9f13f9ac9
ESbeginner <103512348+ESbeginner@users.noreply.github.com>
2022-04-12 17:00:08 +0200

    Update Código.py
    descripción del primer código

--- Código.py
+++ Código.py
@@ -1,1 +1,11 @@
+import pandas as pd #en esta primera línea estamos importando la librería, pero con un nombre más corto, así se nos hace menos pesado hacer uso de esta librería
+data = pd.read_csv("Estacions_de_rec_rrega_per_a_vehicle_el_ctric_a_Catalunya.csv") #la función read_csv() sirve, pues para eso, leer archivos csv. Nótese que para hacer
+                                                                                    #hacer uso de una función dentro de pandas hay que usar el formato
+                                                                                    # {nombre de librería}.{nombre de la función}. Esto se debe a que las librerías no son
+                                                                                    # más que un archivo de python con un <<objeto>>, y este a su vez tiene funciones
+                                                                                    # definidas dentro de sí
+df=pd.DataFrame(data) #Lo que hacemos con esta función es cojer el archivo csv, ahora conocido como <<data>>  organizarlo siguiendo un patrón tipo matriz MxN
+df.T #Esta función solo transpone la matriz que acabamos de crear
 
+if __name__ == "__main__": # esta sección se encarga de que si somos nosotros lo que ejecutamos el código, se active el condicionante if==True
+  print(df) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
