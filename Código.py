2704fb9c694cf7c5dff6ef93f527572c19a43487
ESbeginner <103512348+ESbeginner@users.noreply.github.com>
2022-04-16 17:11:45 +0200

    Update Código.py

--- Código.py
+++ Código.py
@@ -11,3 +11,7 @@ if __name__ == "__main__": # esta sección se encarga de que si somos nosotros l
   print(df) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
 # PROMOTOR-GESTOR, ACCES, TIPUS VELOCITAT, TIPUS CONNEXIÓ, LATITUD, LONGITUD, DESIGNACIÓ-DESCRIPTIVA, POTENCIA, TIPUS DE CORRENT
 # IDENTIFICADOR, ADREÇA, PROVINCIA, CODIPROV, MUNICIPI, NPLACES ESTACIÓ, TIPUS VEHICLE, Columna amb georeferència POINT.
+
+'''
+ACCES, PROVINCIA, MUNICIPI, NPLACES ESTACIÓ
+'''
