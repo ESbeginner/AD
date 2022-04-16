c00c5948e6f79c1886622d53b3013bc05130e4fd
ESbeginner <renatto.guiusseppe.epralta@estudiantat.upc.edu>
2022-04-16 19:19:09 +0200

    9

--- Código.py
+++ Código.py
@@ -1,15 +1,13 @@
-import pandas as pd #en esta primera línea estamos importando la librería, pero con un nombre más corto, así se nos hace menos pesado hacer uso de esta librería
-data = pd.read_csv("Estacions_de_rec_rrega_per_a_vehicle_el_ctric_a_Catalunya.csv") #la función read_csv() sirve, pues para eso, leer archivos csv. Nótese que para hacer
-                                                                                    #hacer uso de una función dentro de pandas hay que usar el formato
-                                                                                    # {nombre de librería}.{nombre de la función}. Esto se debe a que las librerías no son
-                                                                                    # más que un archivo de python con un <<objeto>>, y este a su vez tiene funciones
-                                                                                    # definidas dentro de sí
-df=pd.DataFrame(data) #Lo que hacemos con esta función es cojer el archivo csv, ahora conocido como <<data>>  organizarlo siguiendo un patrón tipo matriz MxN
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+data = pd.read_csv("Estacions_de_rec_rrega_per_a_vehicle_el_ctric_a_Catalunya.csv")
+df=pd.DataFrame(data)
 accesos = df["ACCES"]
 provincia = df["PROVINCIA"]
 municipi = df["MUNICIPI"]
 nplazas = df["NPLACES ESTACIÓ"]
-AD1 = pd.DataFrame([accesos,provincia,municipi,nplazas]).T #
+AD1 = pd.DataFrame([accesos,provincia,municipi,nplazas]).T
 AD2 = AD1.dropna()
 
 AD3 = AD2.reset_index(drop=True)
@@ -31,6 +29,7 @@ for i in range(len(temp)):
         nindex.append(i)
 
 AD3 = AD3.drop(AD3.index[nindex])
+
 AD3 = AD3.reset_index(drop=True)
 
 
