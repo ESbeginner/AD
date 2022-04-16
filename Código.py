7a64cd4ed31ac8401341b35ca8cd74a4c9ade5ab
ESbeginner <renatto.guiusseppe.epralta@estudiantat.upc.edu>
2022-04-16 19:15:14 +0200

    8

--- Código.py
+++ Código.py
@@ -17,13 +17,29 @@ temp = [] # lo que vamos a hacer ahora es buscar los valores de nplaces que
           # no seran trabajables posteriormente, como las sumas
 for i in AD3["NPLACES ESTACIÓ"]:
     try:
-        AD3['NPLACES ESTACIÓ'] = AD3['NPLACES ESTACIÓ'].astype(int)
+        AD3.loc[AD3["NPLACES ESTACIÓ"] == i, "NPLACES ESTACIÓ"] = int(i)
     except ValueError:
         1+1
+for i in AD3["NPLACES ESTACIÓ"]:
+    try:
+        temp.append(i)
+    except ValueError:
+        pass
+nindex = []
+for i in range(len(temp)):
+    if type(temp[i]) == str:
+        nindex.append(i)
+
+AD3 = AD3.drop(AD3.index[nindex])
+AD3 = AD3.reset_index(drop=True)
+
+
+
 
 if __name__ == "__main__": # esta sección se encarga de que si somos nosotros lo que ejecutamos el código, se active el condicionante if==True
   print(AD3) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
   print(temp)
+  print(nindex)
 # PROMOTOR-GESTOR, ACCES, TIPUS VELOCITAT, TIPUS CONNEXIÓ, LATITUD, LONGITUD, DESIGNACIÓ-DESCRIPTIVA, POTENCIA, TIPUS DE CORRENT
 # IDENTIFICADOR, ADREÇA, PROVINCIA, CODIPROV, MUNICIPI, NPLACES ESTACIÓ, TIPUS VEHICLE, Columna amb georeferència POINT.
 
