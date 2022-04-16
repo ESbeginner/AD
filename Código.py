ef4f849d21381f0239b54f332849bff3ceac6178
ESbeginner <renatto.guiusseppe.epralta@estudiantat.upc.edu>
2022-04-16 18:01:25 +0200

    1

--- Código.py
+++ Código.py
@@ -5,13 +5,20 @@ data = pd.read_csv("Estacions_de_rec_rrega_per_a_vehicle_el_ctric_a_Catalunya.cs
                                                                                     # más que un archivo de python con un <<objeto>>, y este a su vez tiene funciones
                                                                                     # definidas dentro de sí
 df=pd.DataFrame(data) #Lo que hacemos con esta función es cojer el archivo csv, ahora conocido como <<data>>  organizarlo siguiendo un patrón tipo matriz MxN
-df=df.T #Esta función solo transpone la matriz que acabamos de crear
+accesos = df["ACCES"]
+provincia = df["PROVINCIA"]
+municipi = df["MUNICIPI"]
+nplazas = df["NPLACES ESTACIÓ"]
+AD = pd.DataFrame(["accesos","provincia"])
 
 if __name__ == "__main__": # esta sección se encarga de que si somos nosotros lo que ejecutamos el código, se active el condicionante if==True
-  print(df) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
+  print(AD) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
 # PROMOTOR-GESTOR, ACCES, TIPUS VELOCITAT, TIPUS CONNEXIÓ, LATITUD, LONGITUD, DESIGNACIÓ-DESCRIPTIVA, POTENCIA, TIPUS DE CORRENT
 # IDENTIFICADOR, ADREÇA, PROVINCIA, CODIPROV, MUNICIPI, NPLACES ESTACIÓ, TIPUS VEHICLE, Columna amb georeferència POINT.
 
 '''
-ACCES, PROVINCIA, MUNICIPI, NPLACES ESTACIÓ
+ACCES(público, privado), PROVINCIA, MUNICIPI, NPLACES ESTACIÓ
+Todas las combinaciones de NPLACES son
+https://aprendeconalf.es/docencia/python/manual/pandas/
+
 '''
