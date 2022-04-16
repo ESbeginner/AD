2f5b9dc5c72d948ca717fcf54eda8a10efdbd29f
AlexQuiereOfertas <alex.lopez.nadal@estudiantat.upc.edu>
2022-04-16 18:20:55 +0200

    4

--- Código.py
+++ Código.py
@@ -12,8 +12,10 @@ nplazas = df["NPLACES ESTACIÓ"]
 AD1 = pd.DataFrame([accesos,provincia,municipi,nplazas]).T #
 AD2 = AD1.dropna()
 
+AD3 = AD2.reset_index()
+
 if __name__ == "__main__": # esta sección se encarga de que si somos nosotros lo que ejecutamos el código, se active el condicionante if==True
-  print(AD2) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
+  print(AD3) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
 # PROMOTOR-GESTOR, ACCES, TIPUS VELOCITAT, TIPUS CONNEXIÓ, LATITUD, LONGITUD, DESIGNACIÓ-DESCRIPTIVA, POTENCIA, TIPUS DE CORRENT
 # IDENTIFICADOR, ADREÇA, PROVINCIA, CODIPROV, MUNICIPI, NPLACES ESTACIÓ, TIPUS VEHICLE, Columna amb georeferència POINT.
 
