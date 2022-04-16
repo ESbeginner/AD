184d8e8734b6ee04a9b1ea95dab79c1f43d449c2
ESbeginner <renatto.guiusseppe.epralta@estudiantat.upc.edu>
2022-04-16 19:22:38 +0200

    11

--- Código.py
+++ Código.py
@@ -37,6 +37,8 @@ import numpy as np
 
 
 
+################################################################################
+#Testeo
 if __name__ == "__main__": # esta sección se encarga de que si somos nosotros lo que ejecutamos el código, se active el condicionante if==True
   print(AD3) # Con esto podremos ver los datos. Para ver solo los 5 primeros es recomendable usar la función Head()
 # PROMOTOR-GESTOR, ACCES, TIPUS VELOCITAT, TIPUS CONNEXIÓ, LATITUD, LONGITUD, DESIGNACIÓ-DESCRIPTIVA, POTENCIA, TIPUS DE CORRENT
